8b46078bcf80761d6398dd85143292cd15719644
Manuel mitasch <manuel@mitasch.com>
2019-02-05 17:47:59 +0100

    Bringing awesome smooth color streaming to ESP8266

--- Firmware/BentoFlow/DeviceSettings.h
+++ Firmware/BentoFlow/DeviceSettings.h
@@ -1,12 +1,16 @@
 #ifndef DEVICESETTINGS_H
 #define DEVICESETTINGS_H
 
+#if USE_OSC
+#include <OSCMessage.h>
+#endif
 
 class DeviceSettings
 {
 public :
 
   static uint32_t deviceID;
+  static uint32_t propID; //
   
   DeviceSettings()
   {
@@ -23,8 +27,35 @@ public :
     Serial.println(deviceID);
     #endif
   }
+  
+  #if USE_OSC
+    bool handleMessage(OSCMessage &msg)
+    {
+      if (msg.match("/settings/propID"))
+      {
+        if (msg.size() > 0 && msg.isInt(0))
+        {
+          propID = msg.getInt(0);
+#if SERIAL_DEBUG
+          Serial.println("New PropID : " + String(propID));
+#endif
+        } else
+        {
+#if SERIAL_DEBUG
+          Serial.println("Bad /setings/propID message format");
+#endif
+        }
+
+        return true;
+      }
+
+      return false;
+
+    }
+#endif
 };
 
 uint32_t DeviceSettings::deviceID = 0;
+uint32_t DeviceSettings::propID = 0;
 
 #endif //DEVICESETTINGS_H
