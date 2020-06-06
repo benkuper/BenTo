9eb614e6d58ae136e5742c9c98c571bc42c07d9e
Ben Kuper <bkuperberg@gmail.com>
2020-06-06 15:28:04 +0200

    1.2.3b1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BenTo";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.2.2";
-    const int          versionNumber  = 0x10202;
+    const char* const  versionString  = "1.2.3b1";
+    const int          versionNumber  = 0x10203;
 }
 #endif
