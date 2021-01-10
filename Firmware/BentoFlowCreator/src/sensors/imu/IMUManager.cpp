e82ee9b9dfcac059324446874c95ea7e5b69477d
Ben Kuper <bkuperberg@gmail.com>
2021-01-10 14:01:05 +0100

    cleanup

--- Firmware/BentoFlowCreator/src/sensors/imu/IMUManager.cpp
+++ Firmware/BentoFlowCreator/src/sensors/imu/IMUManager.cpp
@@ -1,11 +1,11 @@
 #include "IMUManager.h"
 
-const String IMUEvent::eventNames[IMUEvent::TYPES_MAX] { "orientation", "accel", "gyro", "linearAccel", "gravity", "shock", "flatfall", "calibration"};
+const String IMUEvent::eventNames[IMUEvent::TYPES_MAX]{"orientation", "accel", "gyro", "linearAccel", "gravity", "shock", "flatfall", "calibration"};
 
 IMUManager::IMUManager() : Component("imu"),
 #ifdef HAS_IMU
                            bno(55),
-#endif                          
+#endif
                            isConnected(false),
                            isEnabled(false),
                            orientationSendTime(20), //50fps
@@ -18,7 +18,6 @@ IMUManager::IMUManager() : Component("imu"),
   bno.setAxisSign(Adafruit_BNO055::IMU_REMAP_SIGN);
   bno.setMode(Adafruit_BNO055::OPERATION_MODE_NDOF);
 #endif
-
 }
 
 IMUManager::~IMUManager()
@@ -30,7 +29,8 @@ void IMUManager::init()
 #ifdef HAS_IMU
 
   NDBG("Init");
-  if(isConnected) return;
+  if (isConnected)
+    return;
 
   Wire.begin(SDA_PIN, SCL_PIN);
 
@@ -51,24 +51,24 @@ void IMUManager::init()
   isConnected = true;
   NDBG("Imu is connected.");
 #endif
-
 }
 
 void IMUManager::update()
 {
-  if(!isEnabled || !isConnected) return;
+  if (!isEnabled || !isConnected)
+    return;
 
 #ifdef HAS_IMU
   imu::Quaternion q = bno.getQuat();
   q.normalize();
-  
+
   //float temp = q.x();  q.x() = -q.y();  q.y() = temp;
   //q.z() = -q.z();
   imu::Vector<3> euler = q.toEuler();
-  orientation[0] = euler.x() * 180/PI;  //Yaw
-  orientation[1] = euler.y() * 180/PI;  //Pitch
-  orientation[2] = euler.z() * 180/PI;  //Roll
-  
+  orientation[0] = euler.x() * 180 / PI; //Yaw
+  orientation[1] = euler.y() * 180 / PI; //Pitch
+  orientation[2] = euler.z() * 180 / PI; //Roll
+
   imu::Vector<3> laccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
   linearAccel[0] = laccel.x();
   linearAccel[1] = laccel.y();
@@ -100,35 +100,37 @@ void IMUManager::update()
     timeSinceOrientationLastSent = curTime;
   }
 #endif
-
 }
 
 void IMUManager::setEnabled(bool value)
 {
-  if(isEnabled == value) return;
+  if (isEnabled == value)
+    return;
   isEnabled = value;
 }
 
 bool IMUManager::handleCommand(String command, var *data, int numData)
 {
-  if(checkCommand(command, "enabled", numData, 1))
+  if (checkCommand(command, "enabled", numData, 1))
   {
     setEnabled(data[0].intValue());
     return true;
-  } else if(checkCommand(command, "updateRate", numData, 1))
+  }
+  else if (checkCommand(command, "updateRate", numData, 1))
   {
-    orientationSendTime = 1000/data[0].intValue();
+    orientationSendTime = 1000 / data[0].intValue();
     return true;
-  } else if (checkCommand(command, "calibrationStatus", numData, 0))
+  }
+  else if (checkCommand(command, "calibrationStatus", numData, 0))
   {
     uint8_t system, gyro, accel, mag;
     system = gyro = accel = mag = 0;
     bno.getCalibration(&system, &gyro, &accel, &mag);
 
-    calibration[0] = (float) system;
-    calibration[1] = (float) gyro;
-    calibration[2] = (float) accel;
-    calibration[3] = (float) mag;
+    calibration[0] = (float)system;
+    calibration[1] = (float)gyro;
+    calibration[2] = (float)accel;
+    calibration[3] = (float)mag;
 
     sendEvent(IMUEvent(IMUEvent::CalibrationStatus, calibration, 4));
     return true;
