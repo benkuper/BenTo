91bedde28da9750091f30f588e0ad98adfc08aac
Manuel Mitasch <manuel@mitasch.com>
2021-04-12 17:43:52 +0200

    Reworked battery level
    + added basic battery calibration (maxValue adapts to highest measured value)

--- Firmware/BentoFlowCreator/src/sensors/battery/BatteryManager.cpp
+++ Firmware/BentoFlowCreator/src/sensors/battery/BatteryManager.cpp
@@ -1,6 +1,6 @@
 #include "BatteryManager.h"
 
-const String BatteryEvent::eventNames[BatteryEvent::TYPES_MAX]{"level", "voltage", "rawValue", "criticalLevel", "charging"};
+const String BatteryEvent::eventNames[BatteryEvent::TYPES_MAX]{"level", "voltage", "rawValue", "criticalLevel", "charging", "reset"};
 
 BatteryManager::BatteryManager() : Component("battery"),
                                    voltage(4.1f),
@@ -18,7 +18,24 @@ void BatteryManager::init()
 
 #ifdef BATTERY_PIN
     analogSetPinAttenuation(BATTERY_PIN, ADC_0db);
+
+#ifdef USE_PREFERENCES
+    prefs.begin(name.c_str());
+    setMax(prefs.getInt("max", defaultMaxVal), false);
+    prefs.end();
+#elif defined USE_SETTINGS_MANAGER
+    //init once with a json if it doesn't exist yet
+    prefs.readSettings(String("/" + name + ".json").c_str());
+    float max = prefs.getInt("max", defaultMaxVal);
+    prefs.loadJson(String("{\"max\":\"" + String(max) + "\"}").c_str());
+    prefs.writeSettings(String("/" + name + ".json").c_str());
+
+    //actually read the data
+    prefs.readSettings(String("/" + name + ".json").c_str());
+    setMax(prefs.getInt("max", defaultMaxVal), false);
 #endif
+
+#endif // BATTERY_PIN
 }
 
 void BatteryManager::update()
@@ -26,8 +43,10 @@ void BatteryManager::update()
 #ifdef BATTERY_PIN
     long curTime = millis();
     rawValue = analogRead(BATTERY_PIN);
-    value = (rawValue - minVal)*1.0f / (maxVal-minVal);
-    voltage = (rawValue * 10 / 4.0f) / 1000;
+
+    updateValue(rawValue); 
+    updateMax(rawValue); 
+    voltage = (rawValue - defaultMinVal) * (3.8 - 3.2) / (defaultMaxVal - defaultMinVal) + 3.2;
         
     if(curTime > timeSinceLastBatterySent + batterySendTime)
     {
@@ -37,7 +56,7 @@ void BatteryManager::update()
         timeSinceLastBatterySent = curTime;
     }
 
-    bool batteryIsOK = value >= criticalBatteryThreshold;
+    bool batteryIsOK = value > criticalBatteryThreshold;
     if(batteryIsOK) 
     {
         timeAtCriticalBattery = 0;
@@ -52,4 +71,70 @@ void BatteryManager::update()
         }
     }
 #endif
+}
+
+
+void BatteryManager::updateMax(int currentValue) 
+{
+    if (currentValue - 10 > maxVal) {
+        setMax(currentValue - 10, true);
+    } 
+}
+
+
+void BatteryManager::setMax(int newMax, bool save)
+{
+    maxVal = max(newMax, 0);
+
+    if (save)
+    {
+#ifdef USE_PREFERENCES
+        prefs.begin(name.c_str());
+        prefs.putInt("max", maxVal);
+        prefs.end();
+#elif defined USE_SETTINGS_MANAGER
+        prefs.readSettings(String("/" + name + ".json").c_str());
+        prefs.setInt("max", maxVal);
+#endif
+    }
+}
+
+
+void BatteryManager::resetMax() 
+{
+    maxVal = defaultMaxVal;
+    setMax(defaultMaxVal, true);
+
+    sendEvent(BatteryEvent(BatteryEvent::Reset, maxVal));
+}
+
+
+void BatteryManager::updateValue(int newValue) 
+{
+    long sum = 0L;
+    prevRawValues[curPos] = newValue;
+
+    for (int i = 0 ; i < PREV_VALUES_SIZE; i++) 
+    {
+        sum += prevRawValues[i];
+    }
+
+    int avgRawValue = (int) (((float) sum) / PREV_VALUES_SIZE);
+    value = (avgRawValue - minVal)*1.0f / (maxVal-minVal);
+
+    curPos = (curPos+1) % PREV_VALUES_SIZE;
+}
+
+
+bool BatteryManager::handleCommand(String command, var *data, int numData)
+{
+#ifdef BATTERY_PIN
+    if (checkCommand(command, "reset", numData, 0))
+    {
+        resetMax();
+        return true;
+    }
+#endif
+
+    return false;
 }
