e02c02d121f3718fea092ef3d1ffc74f2d66b31e
Ben Kuper <bkuperberg@gmail.com>
2023-10-19 14:10:34 +0200

    fix unix typo in PropIncludes

--- Source/Prop/PropIncludes.h
+++ Source/Prop/PropIncludes.h
@@ -16,7 +16,7 @@
 
 #include "Component/PropComponent.h"
 #include "Prop.h"
-#include "props/Bento/BentoComponentContainer.h"
+#include "props/bento/BentoComponentContainer.h"
 #include "props/bento/BentoProp.h"
 
 #include "Cluster/PropCluster.h"
